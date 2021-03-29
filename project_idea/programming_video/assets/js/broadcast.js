90494a6e008d5dd47fb4edb18be35158c619583a
Jiawen Zhu <jzhu10@mail.sfsu.edu>
2021-03-29 00:52:04 -0500

    adding audio content

--- project_idea/programming_video/assets/js/broadcast.js
+++ project_idea/programming_video/assets/js/broadcast.js
@@ -1,65 +1,39 @@
-TweenMax.set('#ripple-circle circle', { scale: 0.5, transformOrigin: '50% 50%' });
-TweenMax.fromTo('#svg-line', 2, { x: -2.5 }, { x: 2.5, repeat: -1, ease: Elastic.easeInOut, yoyo: true });
-TweenMax.fromTo('.loading-image', 2, { scale: 1, autoAlpha: 1 }, { scale: 0.75, autoAlpha: 0.5, transformOrigin: '50% 50%', ease: Bounce.easeIn, yoyo: true, repeat: -1 });
-TweenMax.to('#inner-circ', 2, { rotation: 360, transformOrigin: '50% 50%', repeat: -1 });
-TweenMax.to('#outer-circ', 4, { rotation: -360, transformOrigin: '50% 50%', repeat: -1 });
-$('.audiosvg').hover(function () {
-  hoverCircle = $(this).find('#hover-circle .st4')
-  $(hoverCircle).css({ 'fill': '#7691BA' });
-  TweenMax.to('.play-text', 0.35, { autoAlpha: 1, y: -70, transformOrigin: '50% 50%', ease: Back.easeOut });
-}, function () {
-  $(hoverCircle).css({ 'fill': '#486CA3' });
-  TweenMax.to('.play-text', 0.35, { autoAlpha: 0, y: 0, transformOrigin: '50% 50%', ease: Back.easeIn });
-});
+var music = document.getElementById("music");
+var playButton = document.getElementById("play");
+var pauseButton = document.getElementById("pause");
+var playhead = document.getElementById("elapsed");
+var timeline = document.getElementById("slider");
+var timer = document.getElementById("timer");
+var duration;
+pauseButton.style.visibility = "hidden";
 
-$('.audiosvg').click(function () {
-  var spinDisc = $(this).find('#svg-audio');
-  var rippleCircle = $(this).find('#ripple-circle circle');
-  var activeBox = $(this).prev();
-  TweenMax.set(spinDisc, { rotation: 0, transformOrigin: '50% 50%' });
-  TweenMax.to(spinDisc, 2, { rotation: 360, transformOrigin: '50% 50%', repeat: -1, ease: Linear.easeNone });
-  TweenMax.staggerTo(rippleCircle, 2.1, { scale: 20, transformOrigin: '50% 50%', autoAlpha: 0, repeat: -1, ease: Linear.easeNone }, 0.7);
-  $(activeBox).show();
-  TweenMax.set('.play-text', { autoAlpha: 0, y: 0, transformOrigin: '50% 50%' });
-  TweenMax.set('.pause-text', { autoAlpha: 0, y: 0, transformOrigin: '50% 50%' });
-});
+var timelineWidth = timeline.offsetWidth - playhead.offsetWidth;
+music.addEventListener("timeupdate", timeUpdate, false);
 
-$('.active-box').click(function () {
-  $(this).hide();
-  TweenMax.killTweensOf($(this).next().find('#svg-audio'));
-  TweenMax.killTweensOf($(this).next().find('#ripple-circle circle'));
-  TweenMax.set('#ripple-circle circle', { scale: 0.5, transformOrigin: '50% 50%' });
-  TweenMax.set('.play-text', { autoAlpha: 0, y: 0, transformOrigin: '50% 50%' });
-  TweenMax.set('.pause-text', { autoAlpha: 0, y: 0, transformOrigin: '50% 50%' });
-});
+function timeUpdate() {
+	var playPercent = timelineWidth * (music.currentTime / duration);
+	playhead.style.width = playPercent + "px";
 
-// $('.active-box').hover(function () {
-//   hoverCircle = $(this).parent().find('#hover-circle .st4')
-//   $(hoverCircle).css({ 'fill': '#7691BA' });
-//   TweenMax.to('.pause-text', 0.35, { autoAlpha: 1, y: -70, transformOrigin: '50% 50%', ease: Back.easeOut });
-// }, function () {
-//   $(hoverCircle).css({ 'fill': '#486CA3' });
-//   TweenMax.to('.pause-text', 0.35, { autoAlpha: 0, y: 0, transformOrigin: '50% 50%', ease: Back.easeIn });
-// });
-
-var audioMp3 = new Audio();
-audioMp3.src = '../../resource/audio.mp3';
-function playAudio() {
-  audioMp3.play();
+	var secondsIn = Math.floor(((music.currentTime / duration) / 3.5) * 100);
+	if (secondsIn <= 9) {
+		timer.innerHTML = "0:0" + secondsIn;
+	} else {
+		timer.innerHTML = "0:" + secondsIn;
+	}
 }
 
-function pauseAudio() {
-  audioMp3.pause();
+playButton.onclick = function() {
+	music.play();
+	playButton.style.visibility = "hidden";
+	pause.style.visibility = "visible";
 }
 
-$('.circles').click(function () {
-  TweenMax.to('.socialmedia-overlay', 1, { css: { 'top': 0 }, ease: Bounce.easeOut });
-  TweenMax.to('.fa-angle-up', 0.5, { autoAlpha: 1, ease: Power2.easeOut, delay: 1 });
-  TweenMax.staggerFrom('.socialmedia-content div', 0.5, { scale: 0.1, transformOrigin: '50% 50%', autoAlpha: 0, ease: Back.easeOut, delay: 0.75 }, 0.5);
+pauseButton.onclick = function() {
+	music.pause();
+	playButton.style.visibility = "visible";
+	pause.style.visibility = "hidden";
+}
 
-})
-$('.fa-angle-up').click(function () {
-  TweenMax.to('.socialmedia-overlay', 0.5, { css: { 'top': '-100%' }, ease: Power2.easeInOut });
-  TweenMax.to(this, 0.5, { autoAlpha: 0, ease: Power2.easeOut });
-})
-
+music.addEventListener("canplaythrough", function () {
+	duration = music.duration;
+}, false);
